36f9d46908f912b4e110311969fd0e0d8bb75945
Android-JCPP-Servo <astewart1138@gmail.com>
2022-10-26 10:58:28 -0500

    Create copies from Codecademy

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
 keys.forEach(function(key){
-  notes.push(document.getElementById(key));
+    notes.push(document.getElementById(key));
 })
 
 // Write named functions that change the color of the keys below
@@ -39,18 +39,18 @@ startOver.hidden= true;
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function() {
-  nextOne.hidden = false;
-  startOver.hidden = true;
-   document.getElementById('word-one').innerHTML = 'HAP-';
-  document.getElementById('letter-note-one').innerHTML = 'G';
-  document.getElementById('word-two').innerHTML = 'PY';
-  document.getElementById('letter-note-two').innerHTML = 'G';
-  document.getElementById('word-three').innerHTML = 'BIRTH-';
-  document.getElementById('letter-note-three').innerHTML = 'A';
-  document.getElementById('word-four').innerHTML = 'DAY';
-  document.getElementById('letter-note-four').innerHTML = 'G';
-  document.getElementById('word-five').innerHTML = 'TO';
-  document.getElementById('letter-note-five').innerHTML = 'C';
-  document.getElementById('word-six').innerHTML = 'YOU!';
-  document.getElementById('letter-note-six').innerHTML = 'B';
+    nextOne.hidden = false;
+    startOver.hidden = true;
+    document.getElementById('word-one').innerHTML = 'HAP-';
+    document.getElementById('letter-note-one').innerHTML = 'G';
+    document.getElementById('word-two').innerHTML = 'PY';
+    document.getElementById('letter-note-two').innerHTML = 'G';
+    document.getElementById('word-three').innerHTML = 'BIRTH-';
+    document.getElementById('letter-note-three').innerHTML = 'A';
+    document.getElementById('word-four').innerHTML = 'DAY';
+    document.getElementById('letter-note-four').innerHTML = 'G';
+    document.getElementById('word-five').innerHTML = 'TO';
+    document.getElementById('letter-note-five').innerHTML = 'C';
+    document.getElementById('word-six').innerHTML = 'YOU!';
+    document.getElementById('letter-note-six').innerHTML = 'B';
 }
