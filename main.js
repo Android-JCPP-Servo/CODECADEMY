5ce908d7ed4c602d28742253bf2d67ce1aaaa4c9
Android-JCPP-Servo <astewart1138@gmail.com>
2022-10-26 11:03:32 -0500

    Write function that changes key color

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ keys.forEach(function(key){
 })
 
 // Write named functions that change the color of the keys below
-
+const keyPlay = e => e.target.style.backgroundColor = 'silver';
 
 // Write a named function with event handler properties
 
