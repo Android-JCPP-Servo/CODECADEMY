b28ac4208acdd0b19f6fb3b4bf5ae27d9145d7f7
Android-JCPP-Servo <astewart1138@gmail.com>
2022-10-26 12:20:42 -0500

    Add event listener for nextOne button

--- main.js
+++ main.js
@@ -40,7 +40,14 @@ nextThree.hidden = true;
 startOver.hidden= true;
 
 // Write anonymous event handler property and function for the first progress button
-
+nextOne.onclick = () => {
+    // Change visibility of next buttons
+    nextOne.hidden = true;
+    nextTwo.hidden = false;
+    // Change content of letter-note elements
+    document.getElementById('letter-note-five').innerHTML = 'D';
+    document.getElementById('letter-note-six').innerHTML = 'C';
+};
 
 // Write anonymous event handler property and function for the second progress button
 
