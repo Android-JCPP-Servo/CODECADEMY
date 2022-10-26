723e4fe3d6a8e2b239d6142e665374eff0ed2a14
Android-JCPP-Servo <astewart1138@gmail.com>
2022-10-26 11:54:16 -0500

    Add mouseup event handler

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ const keyReturn = e => {
 }
 
 // Write a loop that runs the array elements through the function
-const eventHandlers = note => note.onmousedown = keyPlay;
+const eventHandlers = note => {
+    note.onmousedown = keyPlay;
+    note.onmouseup = keyReturn;
+}
 
 // These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
