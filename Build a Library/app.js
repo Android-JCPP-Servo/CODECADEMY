bcfb63fd4d709b2f34a041552d410d58bbb98495
Android-JCPP-Servo <astewart1138@gmail.com>
2022-10-31 13:32:41 -0500

    Fix constructor defaults and manipulate Book instance

--- Build a Library/app.js
+++ Build a Library/app.js
@@ -2,8 +2,8 @@ class Media {
     // CONSTRUCTOR
     constructor(title) {
         this._title = title;
-        this._isCheckedOut;
-        this.ratings;
+        this._isCheckedOut = false;
+        this.ratings = [];
     }
 
     // GETTERS
@@ -72,3 +72,9 @@ class Movie extends Media {
 class CD extends Media {}
 
 const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
