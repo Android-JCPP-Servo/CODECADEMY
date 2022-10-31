0258e9c098815a8c451064d4bc0636e7514cb0b9
Android-JCPP-Servo <astewart1138@gmail.com>
2022-10-31 11:06:26 -0500

    Add methods to Media

--- Build a Library/app.js
+++ Build a Library/app.js
@@ -1,10 +1,12 @@
 class Media {
+    // CONSTRUCTOR
     constructor(title) {
         this._title = title;
         this._isCheckedOut;
         this.ratings;
     }
 
+    // GETTERS
     get title() {
         return this._title;
     }
@@ -15,6 +17,19 @@ class Media {
         return this.ratings;
     }
 
+    // METHODS
+    toggleCheckOutStatus() {
+        this._isCheckedOut = !this._isCheckedOut;
+    }
+    getAverageRating() {
+        let ratingAverage = this._ratings.reduce((currentSum, rating) => currentSum + rating, 0) / this._ratings.length;
+        return ratingAverage;
+    }
+    addRating(rating) {
+        this._ratings.push(rating);
+    }
+
+    // SETTERS
     set isCheckedOut(isCheckedOut) {
         this.isCheckedOut = isCheckedOut;
     }
