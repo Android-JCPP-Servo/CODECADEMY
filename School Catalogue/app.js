9991b3cff4bfe7a87d0034388a21be3b9f94a963
Android-JCPP-Servo <astewart1138@gmail.com>
2022-10-31 14:36:45 -0500

    Create and manipulate high school instance

--- School Catalogue/app.js
+++ School Catalogue/app.js
@@ -63,3 +63,7 @@ const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students m
 lorraineHansbury.quickFacts();
 let subs = ['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli'];
 School.pickSubstituteTeacher(subs);
+
+// HIGH SCHOOL INSTANCE
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+alSmith.sportsTeams;
