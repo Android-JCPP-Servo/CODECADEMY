11529de399e93831d4c0b3246fc69ef522f5d7f0
Android-JCPP-Servo <astewart1138@gmail.com>
2022-10-31 14:00:13 -0500

    Create empty School class constructor

---
+++ School Catalogue/app.js
@@ -0,0 +1,16 @@
+class School {
+    // CONSTRUCTOR
+    constructor(name, level, numberOfStudents) {}
+  
+    // GETTERS
+    get name() { return this._name; }
+    get level() { return this._level; }
+    get numberOfStudents() { return this._numberOfStudents; }
+  
+    // METHODS
+    quickFacts() {}
+    pickSubstituteTeacher() {}
+  
+    // SETTERS
+    set numberOfStudents(numberOfStudents) { this._numberOfStudents = numberOfStudents }
+}
