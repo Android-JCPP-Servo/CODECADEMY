7e342f91716f82cc754622e87ebb188a404ba655
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-01 12:01:00 -0500

    Add logic for checking string length

--- WorkAround Explorer/modules/utilities.js
+++ WorkAround Explorer/modules/utilities.js
@@ -1,1 +1,10 @@
-export function formatNumber(number) {}
+export function formatNumber(number) {
+    // Split the value at the decimal point
+    let str = number.toString().split('.');
+
+    // Check length of section before decimal point
+    if (str[0].length >= 4) {
+        // Add comma after every 3 numbers
+        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+}
