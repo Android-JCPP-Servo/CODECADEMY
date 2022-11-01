2710f41e3e0cfa9a1272183a3423a9c343bf4e31
Android-JCPP-Servo <astewart1138@gmail.com>
2022-11-01 12:11:56 -0500

    Add reference note

--- WorkAround Explorer/modules/utilities.js
+++ WorkAround Explorer/modules/utilities.js
@@ -3,6 +3,7 @@ export function formatNumber(number) {
     let str = Math.floor(number).toString().split('.');
 
     // Check length of section before decimal point
+    // Referenced from https://stackoverflow.com/questions/6784894/add-commas-or-spaces-to-group-every-three-digits
     if (str[0].length >= 4) {
         // Add comma after every 3 numbers
         str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
